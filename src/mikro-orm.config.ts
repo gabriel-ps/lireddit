4be9bb74bbcfc6b2b5909264dee46a8c6a1c1244
Gabriel Pereira da Silva <gabriel.gps@outlook.com>
2021-03-22 04:54:00 -0300

    Post migration, insert and find

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,9 +1,13 @@
 import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants'
 import { Post } from './entities/Post'
+import path from 'path'
 
 const config = {
-  migrations: [],
+  migrations: {
+    path: path.join(__dirname, './migrations'),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
   entities: [Post],
   dbName: 'lireddit',
   user: 'root',
